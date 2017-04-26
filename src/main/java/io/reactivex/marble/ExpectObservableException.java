188e76974dad4f4ad22dab0d775e6590e452a0ce
alexvictoor <alexvictoor@gmail.com>
2017-04-26 22:00:01 +0200

    refactor(rxjava2): tiny code clean up

--- src/main/java/io/reactivex/marble/ExpectObservableException.java
+++ src/main/java/io/reactivex/marble/ExpectObservableException.java
@@ -2,10 +2,6 @@ package io.reactivex.marble;
 
 public class ExpectObservableException extends RuntimeException {
 
-    public ExpectObservableException(String message, String caller) {
-        super(message + "\n\n from assertion at " + caller + "\n\n----------------------\n");
-    }
-
     public ExpectObservableException(String message) {
         super(message);
     }
--- src/main/java/io/reactivex/marble/ExpectSubscriptionsException.java
+++ src/main/java/io/reactivex/marble/ExpectSubscriptionsException.java
@@ -2,11 +2,6 @@ package io.reactivex.marble;
 
 public class ExpectSubscriptionsException extends RuntimeException {
 
-    public ExpectSubscriptionsException(String message, String caller) {
-        super(message + "\n\n from assertion at " + caller + "\n\n----------------------\n");
-    }
-
-
     public ExpectSubscriptionsException(String message) {
         super(message);
     }
